9efde6c0708c81bca354fc4968412863a638c9a8
Nebula0999 <nabuleallan1@gmail.com>
2026-05-12 11:43:03 +0300

    implemented user registration

--- ecomm/urls.py
+++ ecomm/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
+from users import urls as user_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include(user_urls)),
 ]
--- users/views.py
+++ users/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from .models import User
-from rest_framework import viewsets
+from rest_framework import viewsets, status, generics
 from .serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import action, api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+class UserRegisterUser(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
